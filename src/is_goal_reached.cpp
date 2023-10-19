bbec45aa14b726fb2eefdd0ec773e2a10940f69f
Simone Micheletti <86918431+SimoneMic@users.noreply.github.com>
2023-10-19 16:19:22 +0200

    Update is_goal_reached.cpp
    Working in simulation

--- src/is_goal_reached.cpp
+++ src/is_goal_reached.cpp
@@ -19,9 +19,9 @@
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "nav2_util/node_utils.hpp"
 
-#include "bt_nav2_ergocub/is_goal_reached.hpp"
+#include "ergocub_nav2_nodes/is_goal_reached.hpp"
 
-namespace bt_nav2_ergocub
+namespace ergocub_nav2_nodes
 {
 
 GoalReachedConditionModded::GoalReachedConditionModded(
@@ -32,21 +32,8 @@ GoalReachedConditionModded::GoalReachedConditionModded(
   global_frame_("map"),
   robot_base_frame_("base_link")
 {
-  node_ = config().blackboard->get<rclcpp::Node::SharedPtr>("node");
-  node_->get_parameter("global_frame", global_frame_);
-  node_->get_parameter("robot_base_frame", robot_base_frame_);
-  RCLCPP_INFO(node_->get_logger(), "[GoalReachedConditionModded] Using global_frame: %s and robot_base_frame: %s", global_frame_, robot_base_frame_);
-
-  nav2_util::declare_parameter_if_not_declared(
-    node_, "goal_reached_tol",
-    rclcpp::ParameterValue(0.25));
-  node_->get_parameter_or<double>("goal_reached_tol", goal_reached_tol_, 0.25);
-  tf_ = config().blackboard->get<std::shared_ptr<tf2_ros::Buffer>>("tf_buffer");
-
-  node_->get_parameter("transform_tolerance", transform_tolerance_);
-  RCLCPP_INFO(node_->get_logger(), "[GoalReachedConditionModded] Using goal_reached_tol: %f and transform_tolerance: %f", goal_reached_tol_, transform_tolerance_);
-  
-  goal_state_pub_ = node_->create_publisher<std_msgs::msg::Bool>("/is_goal_reached/goal_state", 10);
+  getInput("global_frame", global_frame_);
+  getInput("robot_base_frame", robot_base_frame_);
 }
 
 GoalReachedConditionModded::~GoalReachedConditionModded()
@@ -54,8 +41,28 @@ GoalReachedConditionModded::~GoalReachedConditionModded()
   cleanup();
 }
 
+void GoalReachedConditionModded::initialize()
+{
+  node_ = config().blackboard->get<rclcpp::Node::SharedPtr>("node");
+
+  nav2_util::declare_parameter_if_not_declared(
+    node_, "goal_reached_tol",
+    rclcpp::ParameterValue(0.3));
+  node_->get_parameter_or<double>("goal_reached_tol", goal_reached_tol_, 0.20);
+  tf_ = config().blackboard->get<std::shared_ptr<tf2_ros::Buffer>>("tf_buffer");
+
+  node_->get_parameter("transform_tolerance", transform_tolerance_);
+
+  goal_state_pub_=node_->create_publisher<std_msgs::msg::Bool>("/is_goal_reached", 10);
+
+  initialized_ = true;
+}
+
 BT::NodeStatus GoalReachedConditionModded::tick()
 {
+  if (!initialized_) {
+    initialize();
+  }
   std_msgs::msg::Bool msg;
   if (isGoalReached()) {
     msg.data = true;
@@ -72,8 +79,7 @@ bool GoalReachedConditionModded::isGoalReached()
 {
   geometry_msgs::msg::PoseStamped current_pose;
 
-  if (!nav2_util::getCurrentPose(
-      current_pose, *tf_, global_frame_, robot_base_frame_, transform_tolerance_))
+  if (!nav2_util::getCurrentPose(current_pose, *tf_, global_frame_, robot_base_frame_, transform_tolerance_))
   {
     RCLCPP_DEBUG(node_->get_logger(), "Current robot pose is not available.");
     return false;
@@ -83,14 +89,17 @@ bool GoalReachedConditionModded::isGoalReached()
   getInput("goal", goal);
   double dx = goal.pose.position.x - current_pose.pose.position.x;
   double dy = goal.pose.position.y - current_pose.pose.position.y;
-  //RCLCPP_INFO(node_->get_logger(), "[GoalReachedConditionModded] Distance: %f", std::sqrt(dx * dx + dy * dy));
+  std::cout << "[GoalReachedConditionModded] Goal X: " << goal.pose.position.x << " Y: " << goal.pose.position.y << std::endl;
+  std::cout << "[GoalReachedConditionModded] Current Pose X: " << current_pose.pose.position.x << " Y: " << current_pose.pose.position.y << std::endl;
+  std::cout << "[GoalReachedConditionModded] Distance: " << std::sqrt(dx * dx + dy * dy) << std::endl;
+  std::cout << "[GoalReachedConditionModded] Global Frame: " << global_frame_ << " Robot Frame: " << robot_base_frame_ << std::endl;
   return (dx * dx + dy * dy) <= (goal_reached_tol_ * goal_reached_tol_);
 }
 
-}  // namespace bt_nav2_ergocub
+}  // namespace ergocub_nav2_nodes
 
 #include "behaviortree_cpp_v3/bt_factory.h"
 BT_REGISTER_NODES(factory)
 {
-  factory.registerNodeType<bt_nav2_ergocub::GoalReachedConditionModded>("GoalReachedConditionModded");
+  factory.registerNodeType<ergocub_nav2_nodes::GoalReachedConditionModded>("GoalReachedConditionModded");
 }
