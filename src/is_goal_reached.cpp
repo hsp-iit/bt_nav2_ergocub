06872894d3d6a44fbf73afd5cde77abb65106701
SimoneMic <simone_micheletti@outlook.it>
2024-01-08 12:02:15 +0000

    fixed logic bug and added param check, working

--- src/is_goal_reached.cpp
+++ src/is_goal_reached.cpp
@@ -47,24 +47,39 @@ GoalReachedConditionModded::~GoalReachedConditionModded()
 
 void GoalReachedConditionModded::initialize()
 {
+  std::cout << "[GoalReachedConditionModded] init..." << std::endl;
   //yarp.init();
   node_ = config().blackboard->get<rclcpp::Node::SharedPtr>("node");
 
   nav2_util::declare_parameter_if_not_declared(
     node_, "nav_status_port_name",
     rclcpp::ParameterValue("/is_goal_reached_bt/goal_reached:o"));
-  node_->get_parameter_or<std::string>("nav_status_port_name", nav_status_port_name_);
+  node_->get_parameter<std::string>("nav_status_port_name", nav_status_port_name_);
+  if (nav_status_port_name_=="")
+  {
+    nav_status_port_name_ = "/is_goal_reached_bt/goal_reached:o";
+  }
+  std::cout << "[GoalReachedConditionModded] nav_status_port_name: " << nav_status_port_name_ << std::endl;
 
   nav2_util::declare_parameter_if_not_declared(
     node_, "goal_angular_tol",
     rclcpp::ParameterValue(0.14));
-  node_->get_parameter_or<double>("goal_reached_tol", goal_angular_tol_);
-  std::cout << "[GoalReachedConditionModded] goal_reached_tol: " << goal_angular_tol_ << std::endl;
+  node_->get_parameter<double>("goal_angular_tol", goal_angular_tol_);
+  if (goal_angular_tol_ < 0.1)
+  {
+    goal_angular_tol_=0.1;
+  }
+  std::cout << "[GoalReachedConditionModded] goal_angular_tol: " << goal_angular_tol_ << std::endl;
 
   nav2_util::declare_parameter_if_not_declared(
     node_, "goal_reached_tol",
     rclcpp::ParameterValue(0.3));
-  node_->get_parameter_or<double>("goal_reached_tol", goal_reached_tol_, 0.20);
+  node_->get_parameter<double>("goal_reached_tol", goal_reached_tol_);
+  if (goal_reached_tol_ < 0.05)
+  {
+    goal_angular_tol_=0.05;
+  }
+  std::cout << "[GoalReachedConditionModded] goal_reached_tol: " << goal_reached_tol_ << std::endl;
 
   tf_ = config().blackboard->get<std::shared_ptr<tf2_ros::Buffer>>("tf_buffer");
 
@@ -113,12 +128,20 @@ bool GoalReachedConditionModded::isGoalReached()
   tf2::Quaternion q_goal;
   tf2::fromMsg(goal.pose.orientation, q_goal);
   tf2::Quaternion q_pose;
-  tf2::fromMsg(goal.pose.orientation, q_pose);
+  tf2::fromMsg(current_pose.pose.orientation, q_pose);
+  tf2::Matrix3x3 m_g(q_goal);
+  double r_g, p_g, yaw_g;
+  m_g.getRPY(r_g, p_g, yaw_g);
+  tf2::Matrix3x3 m_p(q_pose);
+  double r_p, p_p, yaw_p;
+  m_p.getRPY(r_p, p_p, yaw_p);
+
   tf2::Matrix3x3 m(q_goal*q_pose.inverse());
   double r, p, yaw;
   m.getRPY(r, p, yaw);
   std::cout << "[GoalReachedConditionModded] relative yaw difference rad: " << yaw << " degrees: " << yaw * 180 / M_PI << std::endl;
-  std::cout << "[GoalReachedConditionModded] Goal X: " << goal.pose.position.x << " Y: " << goal.pose.position.y << std::endl;
+  std::cout << "[GoalReachedConditionModded] pose yaw rad: " << yaw_p << " degrees: " << yaw_p * 180 / M_PI << std::endl;
+  std::cout << "[GoalReachedConditionModded] Goal X: " << goal.pose.position.x << " Y: " << goal.pose.position.y << " Yaw: " << yaw_g << std::endl;
   std::cout << "[GoalReachedConditionModded] Current Pose X: " << current_pose.pose.position.x << " Y: " << current_pose.pose.position.y << std::endl;
   std::cout << "[GoalReachedConditionModded] Distance: " << std::sqrt(dx * dx + dy * dy) << std::endl;
   std::cout << "[GoalReachedConditionModded] Global Frame: " << global_frame_ << " Robot Frame: " << robot_base_frame_ << std::endl;
